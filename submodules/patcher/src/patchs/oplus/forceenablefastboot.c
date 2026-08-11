55eb6540431d950da43834208e7fce0fc6e4ba75
shabi <shabi@250.sb>
2026-08-11 21:49:29 +0800

    Fix segfault
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- submodules/patcher/src/patchs/oplus/forceenablefastboot.c
+++ submodules/patcher/src/patchs/oplus/forceenablefastboot.c
@@ -1,11 +1,12 @@
 #include "patchs/oplus/forceenablefastboot.h"
 #include "arm64_inst/utils.h"
 bool patch_fastbootcheck(char* buffer, int32_t size, int32_t global_var_offset) {
-    for (int32_t i = 0; i <= size - 4; i += 4) {
+    if (size < 12) return false;
+    for (int32_t i = 0; i <= size - 12; i += 4) {
         DecodedInst d = decode_at(buffer, i);
         int64_t off0 = calc_adrl_file_offset(buffer, i+4, 0);
-        if (!str_at(buffer, size, off0, "fastboot_unlock_verify error and reboot.")) continue;
         if (off0 < 0) continue;
+        if (!str_at(buffer, size, off0, "fastboot_unlock_verify error and reboot.")) continue;
         if (d.type == INST_CBZ_W || d.type == INST_CBZ_X){//change to B
             write_instr(buffer, i, change_to_b(d.raw));
             printf("Patched fastboot warning jump at 0x%X to always jump, fastboot always enabled\n", i);
